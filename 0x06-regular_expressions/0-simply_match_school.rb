587962e08947ff80020591ee4d2c8ca43baa02d3
Bawek <bawekemekonnen884@gmail.com>
2024-05-07 11:52:33 -0700

    jjj

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(string)
-  regex = /School/
-  matched = string.match(regex)
-  puts matched ? matched[0] : ""
-end
-
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-match_school(ARGV[0])
+puts ARGV[0].scan(/School/).join
